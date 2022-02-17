6e3a8695570a6e86539eafe1cd23ac9a509c04d3
Jonah Halbisen <truffaka04@gmail.com>
2022-02-17 16:04:05 -0500

    its a fork
    GUI stuff

---
+++ project/UI/Bar.gd
@@ -0,0 +1,21 @@
+extends Control
+export(int) var max_health
+#add a space when he isn't looking
+export(Texture) var bar_texture
+#make stuff bigger
+#make everything bigger
+#add scale on canvas layer?
+#Center stuff, no containers
+#make a canvas layer???????
+#aspect ratio is important
+#Christopher says use containers
+#
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
