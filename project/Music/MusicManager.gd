34a6b95ca054e0cf4f53de10be8d5f6d542655b6
jm8 <joshua.sims@hilliardschools.org>
2022-03-08 15:32:29 -0500

    Made sound loop

--- project/Music/MusicManager.gd
+++ project/Music/MusicManager.gd
@@ -1,7 +1,13 @@
 extends AudioStreamPlayer
 
-export(AudioStream) var ambiance
+export(AudioStreamSample) var ambiance
+export(int) var loop_begin
+export(int) var loop_end
 
 func _ready():
 	stream = ambiance
-	playing = true
+	stream.loop_begin = loop_begin
+	stream.loop_end = loop_end
+	stream.loop_mode = stream.LOOP_FORWARD
+	
+	print(stream.loop_mode, " ", stream.loop_begin, " ", stream.loop_end)
